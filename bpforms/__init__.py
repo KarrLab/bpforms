ffb777b8dd47adbed832461f17904bd9ac96feda
Jonathan Karr <jonrkarr@gmail.com>
2019-02-05 01:18:04 -0500

    setting API; adding setuptools to requirements

--- bpforms/__init__.py
+++ bpforms/__init__.py
@@ -3,3 +3,7 @@ import pkg_resources
 # read version
 with open(pkg_resources.resource_filename('bpforms', 'VERSION'), 'r') as file:
     __version__ = file.read().strip()
+
+from .core import (Identifier, IdentifierSet, SynonymSet, Base, BaseSequence,
+                   Alphabet, dna_alphabet, dna_alphabet, protein_alphabet,
+                   BpForm, DnaForm, RnaForm, ProteinForm, get_form)
