55e37c6b90ce99c664916c4e5fb934576c50664c
Jonathan Karr <jonrkarr@gmail.com>
2019-02-08 00:23:35 -0500

    cleaning up SQL reading

--- bpforms/alphabet/test_readingSQL_PL.py
+++ bpforms/alphabet/test_readingSQL_PL.py
@@ -1,73 +1,81 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
- 
+
 engine = create_engine('sqlite:///DNAmod.sqlite', echo=True)
 declBase = declarative_base(engine)
 ########################################################################
+
+
 class Names(declBase):
     """"""
     __tablename__ = 'names'
-    __table_args__ = {'autoload':True}
+    __table_args__ = {'autoload': True}
 
-class Expanded_alphabet(declBase):
+
+class ExpandedAlphabet(declBase):
     """"""
     __tablename__ = 'expanded_alphabet'
-    __table_args__ = {'autoload':True}
- 
+    __table_args__ = {'autoload': True}
+
 #----------------------------------------------------------------------
-def loadSession():
+
+
+def load_session():
     """"""
     metadata = declBase.metadata
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
- 
-if __name__ == "__main__":
-    session = loadSession()
-    with_inchi = session.query(Names).filter(Names.inchi != '[]').all()
-    print(res[1].othernames)
-
 
 # get individual modifications and create bases
-for item in with_inchi:
 
-    if item.nameid:
-        id = session.query(Expanded_alphabet).filter(Expanded_alphabet.nameid == item.nameid).one()
-        idx = 0
-        while id in alphabet.bases:
-            idx+=1
-            id = id+'_'+str(idx)
 
-    chars = id
+def gen_alphabet():
+    session = load_session()
+    with_inchi = session.query(Names).filter(Names.inchi != '[]').all()
+    for item in with_inchi:
 
-    name = item.iupacname
+        if item.nameid:
+            id = session.query(ExpandedAlphabet).filter(ExpandedAlphabet.nameid == item.nameid).one()
+            idx = 0
+            while id in alphabet.bases:
+                idx += 1
+                id = id+'_'+str(idx)
 
-    synonyms = SynonymSet()
-    if item.chebiname:
-        synonyms.add(item.chebiname)
-    if item.othernames:
-        for name in item.othernemes:
-            synonyms.add(item.othernames[name])
+        chars = id
 
-    identifiers = IdentifierSet()
-    if item.nameid:
-        identifiers.add(Identifier('ChEBI ID', item.namid))
-    
-    structure = item.inchi
+        name = item.iupacname
 
-    if chars in alphabet.bases:
-        warnings.warn('Ignoring canonical base {}'.format(chars), UserWarning)
-        continue
+        synonyms = SynonymSet()
+        if item.chebiname:
+            synonyms.add(item.chebiname)
+        if item.othernames:
+            for name in item.othernemes:
+                synonyms.add(item.othernames[name])
 
-    alphabet.bases[chars] = Base(
-        id=chars,
-        name=mod['name'],
-        synonyms=synonyms,
-        identifiers=identifiers,
-        structure=structure,
-        # comments="Modification of {}.".format(mod['originating_base'])
-    )
+        identifiers = IdentifierSet()
+        if item.nameid:
+            identifiers.add(Identifier('ChEBI ID', item.namid))
 
-# return alphabet
-return alphabet
+        structure = item.inchi
+
+        if chars in alphabet.bases:
+            warnings.warn('Ignoring canonical base {}'.format(chars), UserWarning)
+            continue
+
+        alphabet.bases[chars] = Base(
+            id=chars,
+            name=mod['name'],
+            synonyms=synonyms,
+            identifiers=identifiers,
+            structure=structure,
+            # comments="Modification of {}.".format(mod['originating_base'])
+        )
+
+    # return alphabet
+    return alphabet
+
+
+if __name__ == "__main__":
+    gen_alphabet()
